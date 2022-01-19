dfbd4b1e8acace799ff3fc9d210b8a0636931d03
Riccardonasali <riccardo.azzali94@gmail.com>
2022-01-19 11:42:40 +0100

    GUI update with cancel button

--- Calculator/src/view/DefaultView.java
+++ Calculator/src/view/DefaultView.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -20,6 +22,7 @@ public class DefaultView {
 	JPanel mainPanel = new JPanel(new BorderLayout());
 	JTextField field = new JTextField(10);
 	JPanel keyPanel = new JPanel(new GridLayout(BUTTON_TEXTS.length, BUTTON_TEXTS[0].length));
+	JPanel cancelPanel = new JPanel(new GridLayout(1, 1));
 
 	public DefaultView() {
 		field.setBackground(Color.WHITE);
@@ -29,12 +32,27 @@ public class DefaultView {
 			for (int j = 0; j < BUTTON_TEXTS[i].length; j++) {
 				final JButton button = new JButton(BUTTON_TEXTS[i][j]);
 				button.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+				button.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if(isNumeric(button.getText())) {
+							field.setText(button.getText());
+						}
+					}
+				});
 				keyPanel.add(button);
 			}
 		}
 		
+		final JButton  cancelButton = new JButton("C");
+		cancelPanel.add(cancelButton);
+		cancelButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+
+		
 		mainPanel.add(field, BorderLayout.PAGE_START);
 		mainPanel.add(keyPanel, BorderLayout.CENTER);
+		mainPanel.add(cancelPanel, BorderLayout.PAGE_END);
 
 		frame.add(mainPanel);
 		frame.pack();
@@ -42,6 +60,11 @@ public class DefaultView {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-
 	}
+	
+	
+	public static boolean isNumeric(String str) {
+		  return str.matches("-?\\d+(\\.\\d+)?");
+		}
+	
 }
